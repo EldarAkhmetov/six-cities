fcbc4692999771d56f164a0ebd6104c3af21e9ed
Eldar <eldar.works@gmail.com>
2020-01-29 19:13:51 +0700

    fix tests in module5-task1

--- src/components/main/main.test.jsx
+++ src/components/main/main.test.jsx
@@ -6,36 +6,7 @@ import Main from './main';
 Enzyme.configure({adapter: new Adapter()});
 
 it(`Main correctly renders after relaunch`, () => {
-  const props = {
-    offerCards: [
-      {
-        id: 1,
-        previewImage: `img/apartment-01.jpg`,
-        images: [`../img/room.jpg`, `../img/apartment-01.jpg`, `../img/apartment-02.jpg`, `../img/apartment-03.jpg`,
-          `../img/studio-01.jpg`, `../img/apartment-01.jpg`],
-        title: `Beautiful & luxurious studio at great location`,
-        isFavorite: false,
-        isPremium: false,
-        rating: 4.8,
-        type: `apartment`,
-        bedrooms: 3,
-        maxAdults: 4,
-        price: 120,
-        goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Cofee machine`, `Dishwasher`],
-        host: {
-          id: 3,
-          isPro: true,
-          name: `Angelina`,
-          avatarUrl: `../img/avatar-angelina.jpg`,
-        },
-        description: `A quiet cozy and picturesque that hides behind a river by the unique lightness of Amsterdam.`,
-        coordinates: [52.3909553943508, 4.85309666406198],
-      },
-    ],
-
-  };
-
-  const tree = shallow(<Main {...props}/>);
+  const tree = shallow(<Main />);
 
   expect(tree).toMatchSnapshot();
 });
